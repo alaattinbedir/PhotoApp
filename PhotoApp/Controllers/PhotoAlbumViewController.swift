4a3abb57b47394fe8fc6a70e600bc7fdfa7987fb
Alaattin Bedir <alaattinbedir@gmail.com>
2019-03-03 15:41:18 +0300

    amend

--- PhotoApp/Controllers/PhotoAlbumViewController.swift
+++ PhotoApp/Controllers/PhotoAlbumViewController.swift
@@ -333,11 +333,11 @@ extension PhotoAlbumViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets.init(top: 20, left: 20, bottom: 20, right: 20)
+        return UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (Int(UIScreen.main.bounds.size.width) - (numbeOfItemsInRow - 1) * 6 - 40) / numbeOfItemsInRow
+        let width = (Int(UIScreen.main.bounds.size.width) - (numbeOfItemsInRow - 1)) / numbeOfItemsInRow
         return CGSize(width: width, height: width)
     }
     
