22f5ed7f23c48029bd124cc70cdd752d74f9dfd2
Alaattin Bedir <alaattinbedir@gmail.com>
2019-03-03 16:40:04 +0300

    amend

---
+++ PhotoApp/Extensions/UI/UIImageView.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  PhotoApp
+//
+//  Created by Melike Sardogan on 3.03.2019.
+//  Copyright © 2019 Alaattin Bedir. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func setRounded(round: Float) {
+        self.layer.cornerRadius = CGFloat(round)
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor        
+        self.clipsToBounds = true
+    }
+    
+    func setCircled() {
+        self.layer.cornerRadius = (self.frame.width / 2)
+        self.layer.masksToBounds = true
+    }
+}
